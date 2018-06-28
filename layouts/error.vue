84403774f4bd9e5a290db2e07a3f97cf973f9540
yagisuke <yagisuke.0209@gmail.com>
2018-06-29 00:01:45 +0900

    add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <template v-if="error.statusCode === 404">
+      <h1>404 not found</h1>
+    </template>
+    <template v-else>
+      <h1>Error</h1>
+      <p>{{ error.message }}</p>
+    </template>
+    <nuxt-link to="/">Back to home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
