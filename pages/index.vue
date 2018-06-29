6a52d2525af0d74f570788043d0866724081ae3d
yagisuke <yagisuke.0209@gmail.com>
2018-06-30 00:09:57 +0900

    cookie

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <h1>index</h1>
+    <h1>HOME</h1>
+    <p>
+      <nuxt-link to="/login">
+        Move to login
+      </nuxt-link>
+    </p>
   </div>
 </template>
 
-<script>
-export default {
+<style scoped>
+section {
+  margin: 16px;
 }
-</script>
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,43 @@
 <template>
-  <p>router</p>
+  <div>
+    <h1>Login</h1>
+    <p>
+      <button @click="addCredential">
+        Set credential
+      </button>
+      <button @click="removeCredential">
+        Remove credential
+      </button>
+    </p>
+    <p>
+      <a href="/">Move to home</a>
+    </p>
+  </div>
 </template>
+
+<script>
+import Cookies from 'universal-cookie'
+
+let cookies
+
+export default {
+  mounted() {
+    cookies = new Cookies()
+  },
+  methods: {
+    addCredential() {
+      cookies.set('credential', '1')
+    },
+    removeCredential() {
+      cookies.set('credential', '')
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  margin: 16px;
+}
+</style>
+
